4c1d122f078187bb67a336a74e640fca514b887d
Olle Lindgren <lindgrenolle@live.se>
2020-11-24 12:33:43 +0100

    Add requests dependency

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
           'pandas',
-          'xlwt'
+          'xlwt',
+          'requests'
       ],
     classifiers=[
         "Programming Language :: Python :: 3",
