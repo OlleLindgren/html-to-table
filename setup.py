fdc5807ae0e4c68f9e7c217ce0ad1ecf3cb213a1
Olle Lindgren <lindgrenolle@live.se>
2021-01-07 12:04:28 +0100

    rename version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="html-parsing", # Replace with your own username
-    version="0.1.1",
+    version="v0.1.1",
     author="Olle Lindgren",
     author_email="lindgrenolle@live.se",
     description="A package for parsing data from html",
